b62963bf2803a99fcf69bdfff0d16ed2e61ea6fa
soccerKevin <soccerKevint11@gmail.com>
2015-05-08 14:36:23 -0700

    restrict api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, path: nil, constraints: { subdomain: 'api' } do
     resources :stays
-    resources :sitters
-    resources :owners
+    # resources :sitters
+    # resources :owners
   end
 
   root 'static#index'
