b98c5f77176884d3c8dfaee9b263ba28300bcd41
soccerKevin <soccerKevint11@gmail.com>
2015-05-07 18:09:04 -0700

    refactor common test methods to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,4 +5,22 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  def create_sitter
+  	Sitter.create!(name: 'First Sitter', rank: 5)
+  end
+
+  def create_stay(sitter, start_date, end_date)
+  	Stay.create!(
+							rating: 5,
+							review: "Great Sitter! My Dogs seem happy!",
+							start_date: start_date,
+							end_date: end_date,
+							sitter_id: sitter.id
+						)
+  end
+
+  def json(body)
+    JSON.parse(body, symbolize_names: true)
+  end
 end
