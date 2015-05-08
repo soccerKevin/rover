862f87a862b284f522c4fc06653096c27a130a03
soccerKevin <soccerKevint11@gmail.com>
2015-05-08 12:56:49 -0700

    include rank in sitters

--- app/controllers/api/sitters_controller.rb
+++ app/controllers/api/sitters_controller.rb
@@ -1,11 +1,11 @@
 class API::SittersController < ApplicationController
 	def index
 		@sitters = Sitter.includes(:stays)
-		render json: @sitters.as_json(include: :stays), status: :ok
+		render json: @sitters.as_json(include: :stays, methods: :rank), status: :ok
 	end
 
 	def show
 		@sitter = Sitter.includes(:stays).find(params[:id])
-		render json: @sitter.as_json(include: :stays), status: :ok
+		render json: @sitter.as_json(include: :stays, methods: :rank), status: :ok
 	end
 end
