7cc77cc2f47e91bb916acecf15312306b3a967c9
soccerKevin <soccerKevint11@gmail.com>
2015-05-07 18:03:44 -0700

    set up sitters and stays index
    includes related resources with them

--- app/controllers/api/sitters_controller.rb
+++ app/controllers/api/sitters_controller.rb
@@ -1,6 +1,6 @@
 class API::SittersController < ApplicationController
 	def index
-		sitters = Sitter.all
-		render json: sitters.as_json, status: :ok
+		sitters = Sitter.includes(:stays)
+		render json: sitters.as_json(include: :stays), status: :ok
 	end
 end
--- app/controllers/api/stays_controller.rb
+++ app/controllers/api/stays_controller.rb
@@ -1,6 +1,6 @@
 class API::StaysController < ApplicationController
 	def index
-		stays = Stay.all
-		render json: stays.as_json, status: :ok
+		stays = Stay.includes(:sitter)
+		render json: stays.as_json(include: [:sitter]), status: :ok
 	end
 end
