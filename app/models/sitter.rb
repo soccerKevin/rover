77e5a8c4a274fc1eb4d399926029d2a0bfd0ea95
soccerKevin <soccerKevint11@gmail.com>
2015-05-07 22:49:10 -0700

    add rating, system to sitter model

--- app/models/sitter.rb
+++ app/models/sitter.rb
@@ -2,4 +2,21 @@ class Sitter < ActiveRecord::Base
 	validates :name, uniqueness: true
 
 	has_many :stays
+
+	def rank
+		# score * (10 - stays.count) + rating * (stays.count / 10)
+		c = self.stays.count
+		self.score * (10 - c) + self.rating * (c / 10)
+	end
+
+	def score
+		unique_letters = self.name.chars.to_a.uniq.count
+		5 * unique_letters / 26
+	end
+
+	def rating
+		ratings = self.stays.map{|s| s.rating}
+		sum = ratings.inject{|sum, r| sum + r}
+		sum / ratings.count
+	end
 end
