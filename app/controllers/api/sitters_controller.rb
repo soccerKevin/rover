a5985897adbe545db76acb5ee4229cb446d24069
soccerKevin <soccerKevint11@gmail.com>
2015-05-07 18:40:37 -0700

    add show actions to sitters and stays

--- app/controllers/api/sitters_controller.rb
+++ app/controllers/api/sitters_controller.rb
@@ -1,6 +1,11 @@
 class API::SittersController < ApplicationController
 	def index
-		sitters = Sitter.includes(:stays)
-		render json: sitters.as_json(include: :stays), status: :ok
+		@sitters = Sitter.includes(:stays)
+		render json: @sitters.as_json(include: :stays), status: :ok
+	end
+
+	def show
+		@sitter = Sitter.includes(:stays).find(params[:id])
+		render json: @sitter.as_json(include: :stays), status: :ok
 	end
 end
--- app/controllers/api/stays_controller.rb
+++ app/controllers/api/stays_controller.rb
@@ -1,6 +1,11 @@
 class API::StaysController < ApplicationController
 	def index
-		stays = Stay.includes(:sitter)
-		render json: stays.as_json(include: [:sitter]), status: :ok
+		@stays = Stay.includes(:sitter)
+		render json: @stays.as_json(include: [:sitter]), status: :ok
+	end
+
+	def show
+		@stay = Stay.includes(:sitter).find(params[:id])
+		render json: @stay.as_json(include: [:sitter]), status: :ok
 	end
 end
