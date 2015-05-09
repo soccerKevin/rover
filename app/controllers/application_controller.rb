ce80a22802242851fe2de8f052088673497d9106
soccerKevin <soccerKevint11@gmail.com>
2015-05-08 17:34:39 -0700

    move redirect to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
+
+  before_action :assert_api
+
+  def assert_api
+  	unless request.url.to_s.include? 'api'
+			redirect_to subdomain: 'api' and return
+		end
+  end
 end
